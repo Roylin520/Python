ad3c97376b5ae3c3a11f6a2ecbd7251d8932faa6
Roylin520 <vpn520@gmail.com>
2024-02-25 12:10:11 +0800

    0225

---
+++ Selenium/4.瀏覽器導引.py
@@ -0,0 +1,22 @@
+from time import sleep
+from selenium import webdriver
+
+driver = webdriver.Edge()
+
+# get()，前往指定網址，並印出網站名稱及網址
+url = "https://tw.yahoo.com/"
+driver.get(url)
+print(f"網站名稱：{driver.title},網址：{driver.current_url}\n")
+
+sleep(3)
+# 前往另一個網址，並印出網站名稱及網址
+url = "https://www.google.com/"
+driver.get(url)
+print(f"網站名稱：{driver.title},網址：{driver.current_url}\n")
+
+sleep(3)
+# 回上一頁，使用back()方法，並印出網站名稱及網址
+driver.back()
+print(f"使用back()方法，回到上一頁，網站名稱：{driver.title},網址：{driver.current_url}\n")
+
+sleep(3)
